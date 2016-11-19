ec214d5e847d5142c00e6434f633a78c57e6fe38
jdlawrence <jamil.lawrence@gmail.com>
2016-11-18 16:36:13 -0800

    First commit

---
+++ src/core.js
@@ -0,0 +1,53 @@
+import {List, Map} from 'immutable';
+
+export function setEntries(state, entries) {
+  return state.set('entries', List(entries));
+}
+
+function getWinners(vote) {
+  if (!vote) {
+    return [];
+  }
+  const [a, b] = vote.get('pair');
+  const aVotes = vote.getIn(['tally', a], 0);
+  const bVotes = vote.getIn(['tally', b], 0);
+  if (aVotes > bVotes) {
+    return [a];
+  } 
+  else if (aVotes < bVotes) {
+    return [b];
+  } 
+  else {
+    return [a, b];
+  } 
+}
+
+export function next(state) {
+  const entries = state.get('entries')
+  .concat(getWinners(state.get('vote')));
+
+  if (entries.size === 1) {
+    return state.remove('vote')
+    .remove('entries')
+    .set('winner', entries.first());
+  } 
+  else {
+
+    return state.merge({
+      vote: Map({pair: entries.take(2)}),
+      entries: entries.skip(2)
+    });
+  }
+}
+
+export function vote(state, entry) {
+  return state.updateIn(
+    // First parameter is an array of the path that leads
+    // to the last parameter
+    // 2nd parameter is the value if one is not already present
+    // 3rd is the change function 
+    ['vote', 'tally', entry],
+    0,
+    tally => tally + 1
+    );
+}
